c9f4f65c2348b679a03006ff56d8c7c5f9f2024c
Vidya A Chhabria <chhab011@nyad.ece.umn.edu>
2019-12-14 00:30:48 -0600

    Tests run on Docker

--- test/test_aes.tcl
+++ test/test_aes.tcl
@@ -1,8 +1,8 @@
 set test_dir [file dirname [file normalize [info script]]]
 set openroad_dir [file dirname [file dirname [file dirname $test_dir]]]
-set OPDN_DIR [file join "src/OpeNPDN"]
-set opendbpy [file join "build/src/swig/python/opendbpy.py"
-#puts $test_dir
+set OPDN_DIR [file join $test_dir "/OpenROAD/src/OpeNPDN"]
+set opendbpy [file join $openroad_dir "/OpenROAD/build/src/OpenDB/src/swig/python/opendbpy.py"]
+#puts $openroad_dir
 
 #set OPDN_DIR "/home/sachin00/chhab011/tmp/OpenROAD/src/OpeNPDN/"
 #set opendbpy "/home/sachin00/chhab011/OpenDB/build/src/swig/python/opendbpy.py"
@@ -11,10 +11,10 @@ set opendbpy [file join "build/src/swig/python/opendbpy.py"
 #read_liberty ../platforms/nangate45/NangateOpenCellLibrary_typical.lib
 #read_sdc aes/aes.sdc
 
-read_lef  ${OPDN_DIR}/platforms/nangate45/NangateOpenCellLibrary.mod.lef
-read_def ${OPDN_DIR}/test/aes/aes.def
-read_liberty ${OPDN_DIR}/platforms/nangate45/NangateOpenCellLibrary_typical.lib
-read_sdc ${OPDN_DIR}/test/aes/aes.sdc
+read_lef  ../platforms/nangate45/NangateOpenCellLibrary.mod.lef
+read_def aes/aes.def
+read_liberty ../platforms/nangate45/NangateOpenCellLibrary_typical.lib
+read_sdc aes/aes.sdc
 
 
 set checkpoints "./OpeNPDN-Checkpoint-FreePDK45"
